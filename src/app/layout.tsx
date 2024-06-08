eae821d79c8013c85fa313646e533ceb32150b27
gyasi.eth <104705433+barigyasi@users.noreply.github.com>
2024-06-07 19:45:49 -0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { ThirdwebProvider } from "thirdweb/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "thirdweb SDK + Next starter",
+  title: "Public Goods Club",
   description:
-    "Starter template for using thirdweb SDK with Next.js App router",
+    "IYKYK",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { useActiveAccount, ConnectButton } from "thirdweb/react";
 import { getContract, sendAndConfirmTransaction, createThirdwebClient, defineChain } from "thirdweb";
 import { mintWithSignature } from "thirdweb/extensions/erc721";
 import { ThirdwebStorage } from "@thirdweb-dev/storage";
-import Image from "next/image";
 import { createWallet } from "thirdweb/wallets";
 import { baseSepolia } from "thirdweb/chains";
 import AvatarCanvas from "../components/AvatarCanvas";
@@ -129,19 +128,11 @@ export default function Home() {
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
       <div className="py-20">
-        <Header />
-
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            wallets={wallets}
-            chain={defineChain(baseSepolia)}
-            appMetadata={{
-              name: "Avatar",
-              url: "https://example.com",
-            }}
-          />
-        </div>
+        <Header
+          client={client}
+          wallets={wallets}
+          chain={defineChain(baseSepolia)}
+        />
 
         <div className={styles.collectionContainer}>
           <h2 className={styles.ourCollection}>Mint your own Avatar NFT:</h2>
@@ -154,7 +145,7 @@ export default function Home() {
             onChange={(e) => setNftName(e.target.value)}
           />
 
-          <div>
+          <div className={styles.centered}>
             <label>
               <input
                 type="radio"
@@ -185,7 +176,7 @@ export default function Home() {
           </div>
 
           <div className={styles.colorPicker}>
-            <label>Skin Color:</label>
+            <label>Body Color:</label><br/>
             <Sketch
               color={skinColor}
               onChange={(color) => setSkinColor(color.hex)}
@@ -205,72 +196,61 @@ export default function Home() {
 
         {error && <p style={{ color: 'red' }}>{error}</p>}
 
-        {signatureData && (
-          <div>
-            <h2>Signature Data:</h2>
-            <pre>{JSON.stringify(signatureData, null, 2)}</pre>
-          </div>
-        )}
+      
 
-        <ThirdwebResources />
+        
       </div>
     </main>
   );
 }
 
-function Header() {
+interface HeaderProps {
+  client: any;
+  wallets: any[];
+  chain: any;
+}
+
+function Header({ client, wallets, chain }: HeaderProps) {
   return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
+    <header className={`${styles.header} flex flex-col items-center mb-20 md:mb-20`}>
       <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        Avatar Minting App
+        PGC Beta
       </h1>
 
-      <p className="text-zinc-300 text-base">
-        Create and mint your own custom avatars!
-      </p>
+      <div className={""}>
+      <ConnectButton
+        client={client}
+        wallets={wallets}
+        chain={chain}
+        theme={"dark"}
+        appMetadata={{
+          name: "Avatar",
+          url: "https://example.com",
+        }}
+        connectButton={{ label: "Log In or Sign Up" }}
+        connectModal={{
+          size: "wide",
+          title: "Choose Method",
+          titleIcon: "",
+          welcomeScreen: {
+            subtitle:
+              "Log In Or Sign Up To Get Started",
+            title: "PublicGoodsClub",
+            img: {
+              src: "",
+              width: 150,
+              height: 150,
+            },
+          },
+          showThirdwebBranding: false,
+        }}
+      />
+      
+      </div>
     </header>
   );
 }
 
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
 
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
 
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
-    </div>
-  );
-}
 
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
-  );
-}
